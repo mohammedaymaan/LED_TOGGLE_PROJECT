9357071a29379e7eddd43fae96003bb1d6e0b29d
Aymooonaaaa <mohamedayman0103@gmail.com>
2022-08-21 05:16:00 +0200

    ANOTHER CHANGE

--- 02-MCAL/DIO/DIO_config.h
+++ 02-MCAL/DIO/DIO_config.h
@@ -2,6 +2,11 @@
 #define DIO_CONFIG_H
 
 
+#define DIO_CONFIGURED_CHANNLES             (4U)
+/* Number of the configured Dio Ports */
+#define DIO_CONFIGURED_PORTS 				(2U)
 
+/* Number of the configured Dio Groups */
+#define DIO_CONFIGURED_GROUPS 				(2U)
 
 #endif 
